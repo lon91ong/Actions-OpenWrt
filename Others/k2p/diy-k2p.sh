4f64088011917f84a46acc51d1d656c7fd611ebf
lon91ong <lon91ong@gmail.com>
2021-10-12 20:04:06 +0800

    Update diy-k2p.sh

--- Others/k2p/diy-k2p.sh
+++ Others/k2p/diy-k2p.sh
@@ -10,6 +10,13 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+rm -Rf target/linux/{ramips,generic}
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
+
+rm -rf include/kernel-version.mk
+wget -O include/kernel-version.mk https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-version.mk
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
@@ -29,8 +36,23 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/fee
 #sed -i 's/Phicomm K2P/&\n  DEVICE_PACKAGES := -luci-newapi -wpad-openssl kmod-mt7615d_dbdc wireless-tools/1' target/linux/ramips/image/mt7621.mk
 #sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 
-# 修复DHCP服务, 从5.4内核改回4.14内核的resolv.conf路径
-#sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
-
 # custom插件汉化
 #mv feeds/custom/luci-app-turboacc/po/zh_Hans feeds/custom/luci-app-turboacc/po/zh-cn
+
+sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615d luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk
+
+sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+
+rm -Rf files/etc/profile.d/sysinfo.sh
+
+sed -i 's/O2/Os/g' include/target.mk
+
+sed -i '/app_update/d' package/feeds/custom/luci-app-bypass/luasrc/controller/bypass.lua
+
+sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-bypass/Makefile
+
+sed -i 's/ +unzip +lua-maxminddb//' package/feeds/custom/luci-app-bypass/Makefile
+
+sed -i '/status_bottom/d' package/feeds/custom/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
+
+rm -Rf package/feeds/custom/luci-app-bypass/{root/www,root/usr/share/bypass/GeoLite2-Country.mmdb}
