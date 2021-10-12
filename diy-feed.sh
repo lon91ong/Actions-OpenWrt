a03a365077498ea299f1cbc0e20f25e998a4550c
lon91ong <lon91ong@gmail.com>
2021-10-12 08:40:03 +0800

    Update diy-feed.sh

--- diy-feed.sh
+++ diy-feed.sh
@@ -18,9 +18,12 @@
 #sed -i 's/17.01/19.07/1' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git packages https://github.com/coolsnowwolf/packages;openwrt-19.07' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+sed -i '$a src-git packages https://github.com/coolsnowwolf/packages;openwrt-19.07' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git custom https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07' feeds.conf.default
+#sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
