4bf884d43cd3b898dcba3bd05486d39f7de4df3d
lon91ong <lon91ong@gmail.com>
2021-10-13 10:09:35 +0800

    Update diy-k2p.sh

--- Others/k2p/diy-k2p.sh
+++ Others/k2p/diy-k2p.sh
@@ -10,12 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-rm -Rf target/linux/{ramips,generic}
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
+#rm -Rf target/linux/{ramips,generic}
+#svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+#svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
 
-rm -rf include/kernel-version.mk
-wget -O include/kernel-version.mk https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-version.mk
+#rm -rf include/kernel-version.mk
+#wget -O include/kernel-version.mk https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-version.mk
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
@@ -35,24 +35,8 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/fee
 # 修复K2P无线丢失错误配置
 sed -i 's/kmod-mt7615e/& luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk
 # 默认闪存布局
-sed -i 's/15744k/16064k/g' target/linux/ramips/image/mt7621.mk
+#sed -i 's/15744k/16064k/g' target/linux/ramips/image/mt7621.mk
 #sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 
 # custom插件汉化
 #mv feeds/custom/luci-app-turboacc/po/zh_Hans feeds/custom/luci-app-turboacc/po/zh-cn
-
-sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-
-rm -Rf files/etc/profile.d/sysinfo.sh
-
-sed -i 's/O2/Os/g' include/target.mk
-
-sed -i '/app_update/d' package/feeds/custom/luci-app-bypass/luasrc/controller/bypass.lua
-
-sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-bypass/Makefile
-
-sed -i 's/ +unzip +lua-maxminddb//' package/feeds/custom/luci-app-bypass/Makefile
-
-sed -i '/status_bottom/d' package/feeds/custom/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
-
-rm -Rf package/feeds/custom/luci-app-bypass/{root/www,root/usr/share/bypass/GeoLite2-Country.mmdb}
