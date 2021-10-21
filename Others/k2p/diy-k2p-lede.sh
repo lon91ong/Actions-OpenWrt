3c08326d61a4ef28d0b1c2373234dec6f8bffbab
lon91ong <lon91ong@gmail.com>
2021-10-21 08:42:41 +0800

    Update diy-k2p-lede.sh

--- Others/k2p/diy-k2p-lede.sh
+++ Others/k2p/diy-k2p-lede.sh
@@ -26,7 +26,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/feeds/custom/luci-theme-argon
 
 # 修复K2P无线丢失错误配置
-#sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615e luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615e kmod-mt7615-firmware/g' target/linux/ramips/image/mt7621.mk
 # 默认布局 16064k
 sed -i 's/15744/16064/g' target/linux/ramips/image/mt7621.mk
 sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
