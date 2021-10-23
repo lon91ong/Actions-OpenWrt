539bf9848abd343abc248607fa58408a9e65ef98
lon91ong <lon91ong@gmail.com>
2021-10-23 22:56:25 +0800

    Update diy-B70.sh

--- Others/B70/diy-B70.sh
+++ Others/B70/diy-B70.sh
@@ -28,7 +28,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # 机型名称适配
 sed -i 's/HC5962/&-SPI")\n\t\tname="hc5962-spi"\n\t\t;;\n\t\*"&/1' target/linux/ramips/base-files/lib/ramips.sh
 # 网络接口适配 16m/1, 32m/g
-sed -i ':a;N;$!ba;s/hc5962/&|\\\n\t&-spi/1' target/linux/ramips/base-files/etc/board.d/02_network
+# Lienol
+#sed -i ':a;N;$!ba;s/hc5962/&|\\\n\t&-spi/1' target/linux/ramips/base-files/etc/board.d/02_network
+# lede
+sed -i ':a;N;$!ba;s/hc5962/&|\\\n\t&-spi/1' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
 cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
 define Device/hc5962-spi
