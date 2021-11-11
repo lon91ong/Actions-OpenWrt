dea9a02db776994c600c20d5e78962e2956f2393
lon91ong <lon91ong@gmail.com>
2021-11-11 09:29:25 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -20,12 +20,14 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-set
 find target/linux -path "target/linux/*/config-*" | xargs -i sed -i '$a CONFIG_ACPI=y\nCONFIG_X86_ACPI_CPUFREQ=y\n \
 CONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"' {}
 
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+rm -rf feeds/custom/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/custom/luci-theme-argon
+
 #设置16m闪存
 sed -i 's/0x7b0000/0xfb0000/g' target/linux/ramips/dts/PSG1218.dtsi
 sed -i 's/DTS := PSG1218A/&\n\tIMAGE_SIZE := 15744k/g' target/linux/ramips/image/mt7620.mk
-
-# 修改默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 
 # 默认turboacc开启DNS Caching功能
 #sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
@@ -33,4 +35,4 @@ sed -i 's/DTS := PSG1218A/&\n\tIMAGE_SIZE := 15744k/g' target/linux/ramips/image
 sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
 
 # turboacc插件汉化
-mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
+#mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
