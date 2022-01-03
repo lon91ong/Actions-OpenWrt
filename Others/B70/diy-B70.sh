b483aa6f7c0c8e560b9e2edaf05e712c70c74366
lon91ong <lon91ong@gmail.com>
2022-01-03 16:25:22 +0800

    Update diy-B70.sh

--- Others/B70/diy-B70.sh
+++ Others/B70/diy-B70.sh
@@ -28,6 +28,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # 机型名称适配
 #sed -i 's/HC5962/&-SPI")\n\t\tname="hc5962-spi"\n\t\t;;\n\t\*"&/1' target/linux/ramips/base-files/lib/ramips.sh
 
+# 固件添加内核版本号
+sed -i ':a;N;$!ba;s/$(BOARD)/&$(LINUX_VERSION)/1' include/image.mk
+
 # 网络接口适配 16m/1, 32m/g
 sed -i ':a;N;$!ba;s/hc5962/&|\\\n\thiwifi,&-spi/1' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
