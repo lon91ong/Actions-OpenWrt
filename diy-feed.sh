b90b4d85979e5199dbec62f3b29f79662171aa2d
lon91ong <lon91ong@gmail.com>
2022-04-21 08:30:36 +0800

    Update diy-feed.sh

--- diy-feed.sh
+++ diy-feed.sh
@@ -11,7 +11,7 @@
 #
 # Comment a feed source
 #sed -i -r 's/(^src-git packages.*)/#\1/' feeds.conf.default
-#sed -i -r 's/(^src-git luci.*)/#\1/' feeds.conf.default
+sed -i -r 's/(^src-git luci.*)/#\1/' feeds.conf.default
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
@@ -19,6 +19,6 @@
 
 # Add a feed source
 #sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;main' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;18.06-dev' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 sed -i '$a src-git custom https://github.com/kiddin9/openwrt-packages' feeds.conf.default
