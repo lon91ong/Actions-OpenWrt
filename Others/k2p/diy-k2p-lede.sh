6425158fa96c07e8603bc1de1a60d4727097000c
lon91ong <lon91ong@gmail.com>
2022-07-13 15:18:37 +0800

    Update diy-k2p-lede.sh

--- Others/k2p/diy-k2p-lede.sh
+++ Others/k2p/diy-k2p-lede.sh
@@ -23,7 +23,7 @@ CONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"' {}
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 # 切换内核版本为5.10
 #sed -i 's/5.4/5.10/g' target/linux/ramips/Makefile
